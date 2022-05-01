c58d0bb979d508269b6ef1205d3d2a0c192fce5c
himanshi1511 <101582710+himanshi1511@users.noreply.github.com>
2022-05-01 21:00:15 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j,a;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(a=n-i;a>i;a--)
+        cout<<" ";
+        for(j=0;j<i<)
+    }
+}
